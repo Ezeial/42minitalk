ae59f6d2384387686d5c4ebee1ad760e50821003
tglory <tglory@student.42Lyon.fr>
2021-11-16 23:45:48 +0100

    add error message

--- client.c
+++ client.c
@@ -7,17 +7,31 @@
 #include <stdio.h>
 #include "libft.h"
 
-void	send_byte(int pid, unsigned char byte, int position)
+/**
+ * Return -1 if an error has occurred otherwise 0
+ */
+static int	send_byte(int pid, unsigned char byte, int position)
 {
-	if (position > 0)
-	{
-		send_byte(pid, byte / 2, position - 1);
-	}
-	usleep(800);
+	int	ret;
+
+	if (position > 0 && send_byte(pid, byte / 2, position - 1) == -1)
+		return (-1);
+	if (usleep(800) == -1)
+		return (-1);
 	if (byte % 2)
-		kill(pid, SIGUSR2);
+		return (kill(pid, SIGUSR2));
 	else
-		kill(pid, SIGUSR1);
+		return (kill(pid, SIGUSR1));
+}
+
+static void	check_arg(int ac)
+{
+	if (ac < 3)
+	{
+		printf("The 1st must be the PID of the server. " \
+			"The 2nd argument must be a string.\n");
+		exit(1);
+	}
 }
 
 int	main(int ac, char **av)
@@ -25,14 +39,23 @@ int	main(int ac, char **av)
 	int		pid;
 	char	*str;
 
+	check_arg(ac);
 	pid = atoi(av[1]);
 	str = av[2];
 	while (*str)
 	{
 		printf("%c\n", *str);
-		send_byte(pid, *str, 7);
+		if (send_byte(pid, *str, 7) == -1)
+		{
+			printf("An error has occurred when sending the '%c'.", *str);
+			return (1);
+		}
 		str++;
 	}
-	send_byte(pid, 0, 7);
+	if (send_byte(pid, 0, 7) == -1)
+	{
+		printf("An error has occurred when sending the end signal.");
+		return (1);
+	}
 	return (0);
 }
